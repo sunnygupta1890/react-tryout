d63ef97d63d5ea121364daa179e12aa1c160afb5
Sunny Gupta <sunnygupta1890@gmail.com>
2023-01-01 13:19:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      Enter the item name : <br/>
+      <input type="text" />
+    </div>
+  );
+}
