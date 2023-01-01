5f19883e3c24d639ad2544be7c396b8ad729121e
Sunny Gupta <sunnygupta1890@gmail.com>
2023-01-01 11:24:24 +0000

    Modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      Enter the item name : <br/>
+      Enter the item name here : <br/>
       <input type="text" />
     </div>
   );
